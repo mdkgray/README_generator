45d6d023b14651e877e521c13bd60940b0e64e8b
Mackenzie Gray <mdkulen@gmail.com>
2022-07-15 11:52:20 +1000

    npm install and written links to inquirer and fs

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,25 @@
-// TODO: Include packages needed for this application
+// variable for inquirer
+const inquirer = require('inquirer');
+
+// variable for fs
+const fs = require('fs');
+
+// variable for generateMarkdown.js file
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// function to validate user input
+function validateUserInput(value) {
+    if (value != '') {
+        return true;
+    } else {
+        return 'Please enter a response to the question.';
+    }
+}
+
+// function to return license badge and link for README.md file
+function renderLicense(value) {
+    if (value)
+}
 
 // TODO: Create an array of questions for user input
 const questions = [];
