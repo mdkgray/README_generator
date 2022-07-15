b1db621186c8d29f7e4c219591de9cffa4df0702
Mackenzie Gray <mdkulen@gmail.com>
2022-07-15 17:36:51 +1000

    code formatting

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,3 @@
-// // TODO: Create a function that returns a license badge based on which license is passed in
-// // If there is no license, return an empty string
-// function renderLicenseBadge(license) {}
-
-// // TODO: Create a function that returns the license link
-// // If there is no license, return an empty string
-// function renderLicenseLink(license) {}
-
-// // TODO: Create a function that returns the license section of README
-// // If there is no license, return an empty string
-// function renderLicenseSection(license) {}
-
 // function to return license badge and link for README.md file
 function renderLicense(data) {
   if (data === 'Apache 2.0') {
@@ -28,8 +16,6 @@ function renderLicense(data) {
       return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
   }
 };
-
-
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
